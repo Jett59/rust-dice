0b50284d4fae9efa7f543a81c6aa7d6b98a7f8f4
Jett Thompson <jett.thompson.59@gmail.com>
2022-08-10 21:37:36 +1000

    make the numbers more random by initializing the rand multiple times

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ use rand::Rng;
 use tts::Tts;
 
 fn say(text: &str, tts: &mut Tts) {
-    tts.stop();
     tts.speak(text, true).expect("Failed to speak");
 }
 
 fn main() {
     let mut tts = Tts::default().expect("Failed to make the tts");
         say("Welcome to the talking dice app. Press enter to receive a random number.", &mut tts);
-        let mut rng = rand::thread_rng();
         loop {
+            let mut rng = rand::thread_rng();
             let mut input = String::new();
             std::io::stdin().read_line(&mut input).expect("Failed to read input");
             let number = rng.gen_range(1..=6);
