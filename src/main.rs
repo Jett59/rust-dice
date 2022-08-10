334c52cb909b2e6deced48ea188101177bb9edd8
Jett Thompson <jett.thompson.59@gmail.com>
2022-08-10 21:09:57 +1000

    add the talking dice app

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use rand::Rng;
+use tts::Tts;
+
+fn say(text: &str, tts: &mut Tts) {
+    tts.speak(text, true).expect("Failed to speak");
+}
+
+fn main() {
+    let mut tts = Tts::default().expect("Failed to make the tts");
+        say("Welcome to the talking dice app. Press enter to receive a random number.", &mut tts);
+        let mut rng = rand::thread_rng();
+        loop {
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).expect("Failed to read input");
+            let number = rng.gen_range(1..=6);
+            say(&format!("{}", number), &mut tts);
+        }
+}
